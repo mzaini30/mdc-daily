a49da6d8bc63a668a172b0505c06dce91e2614aa
mzaini30 <muhzaini30@gmail.com>
2018-02-12 01:26:13 +0000

    oke

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-hari_ini = new Date();
-hari_ini = hari_ini.getTime();
-document.writeln(hari_ini);
-document.writeln(gambar.length);
+var intQuoteCount = gambar.length; // The number of quotes in your library
+var dtNow = new Date();
+var intTZOffset = dtNow.getTimezoneOffset() * 60000; // automatically adjust for user timezone
+var intNow = dtNow.getTime() - intTZOffset;
+var intDay = Math.floor(intNow / 86400000); // The number of 'local' days since Jan 1, 1970
+var intQuoteToDisplay = intDay % intQuoteCount;
+
+document.writeln(intQuoteToDisplay);
